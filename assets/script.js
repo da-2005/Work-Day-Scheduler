4f1366b0aa97de978be40f00bc83fbc3edbc81ba
kdot <alvarezderrick0@gmail.com>
2021-11-23 21:55:21 -0800

    removed card height

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,10 @@
 var today = moment();
 $('#currentDay').text(today.format('dddd, MMMM Do YYYY'));
+
+$(document).ready(function(){
+    $('.saveBtn').on('click', function(){
+        var text = $(this).siblings('.description').val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+    })
+})
