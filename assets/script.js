80d3cd35fefabee47ae53175aa16956050ac7e09
kdot <alvarezderrick0@gmail.com>
2021-11-24 21:26:12 -0800

    removeClass and addClass to color code time blocks

--- assets/script.js
+++ assets/script.js
@@ -8,3 +8,38 @@ $(document).ready(function(){
         localStorage.setItem(time, text);
     })
 })
+
+function timeTracker() {
+    var timeNow = moment().hour();
+    $(".time-block").each(function() {
+        var blockTime = parseInt($(this).attr("id").split("hour"));
+
+        if (blockTime < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+
+$('#9am .description').val(localStorage.getItem('9am'));
+$('#10am .description').val(localStorage.getItem('10am'));
+$('#11am .description').val(localStorage.getItem('11am'));
+$('#12pm .description').val(localStorage.getItem('12pm'));
+$('#1pm .description').val(localStorage.getItem('1pm'));
+$('#2pm .description').val(localStorage.getItem('2pm'));
+$('#3pm .description').val(localStorage.getItem('3pm'));
+$('#4pm .description').val(localStorage.getItem('4pm'));
+$('#5pm .description').val(localStorage.getItem('5pm'));
+
+timeTracker();
